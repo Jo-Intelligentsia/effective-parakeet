d19f3f58ceb4e9c03af5e79ba834883c674e5c64
meni2000 <meni2000@naver.com>
2023-04-19 14:59:58 +0900

    add allowed hosts

--- crud/settings.py
+++ crud/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['port-0-effective-parakeet-1maxx2algna0tik.sel3.cloudtype.app']
 
 
 # Application definition
